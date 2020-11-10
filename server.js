998608813103d648a7ae5778cacb78f503baa276
Lorenzo Biagi <you@example.com>
2020-11-10 19:21:19 +0100

    aggiunto tutto

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+
+//express.render() cerca il file già nelle views
+var fakeApi= () => 'Faker'
+app.get('/', function(req, res) {
+    var proPlayer=fakeApi();
+    res.render('pages/index',
+    {proPlayer:proPlayer,}
+)}
+);
+
+
+app.get('/other', function(req, res) {
+    res.render('pages/other',);
+});
+var port=8080
+
+app.listen(port);
+console.log('Server is listening on port',port);
