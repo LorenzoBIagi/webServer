c1b5c48e4db237e1f1c6b81b5c0d1ce723e30a83
Lorenzo Biagi <you@example.com>
2020-12-16 07:13:51 +0100

    aggiunta champions

--- server.js
+++ server.js
@@ -12,14 +12,30 @@ const menu =[
         content:'Here you can explore and discover my favourite champions and explore my gaming style!'
     },
     {
+        name:'Champions',
+        url:'/champions',
+        content:'Here you can see my favourite champions'
+    },
+    {
         name:'Other',
         url:'/other',
         content:'That\'s the content of other\'s page, have fun'
     },
+   
 ]
+var champions={
+   
+    hated:[
+        'Yasuo','Zoe','Mundo','Darius','Fizz'
+    ],
+    loved:[
+        'LeBlanc','Lux','Teemo','Kassadin','Jarvan IV'
+    ]
+    
+}
 
 var fakeApi= () => 'Faker'
-//views
+//Routes
 
 //Home
 app.get('/', function(req, res) {
@@ -27,8 +43,9 @@ app.get('/', function(req, res) {
     var data={
         page:'Home',
         url:req.url,
-        menu:menu
+        menu:menu,
     }
+  
 
     var proPlayer=fakeApi();
 
@@ -41,13 +58,34 @@ app.get('/', function(req, res) {
 )}
 );
 
+//Champions
+app.get('/champions', function(req, res) {
+
+    var data={
+        page:'Champions',
+        url:req.url,
+        menu:menu,
+        champions:champions
+    }
+  
+
+    //express.render() cerca il file già nelle views
+    res.render('pages/champions',
+    {
+    data:data 
+    }
+)}
+);
+
 //Other
 app.get('/other', function(req, res) {
 
     var data={
         page:'Other',
         url:req.url,
-        menu:menu
+        menu:menu,
+        champions:champions
+        
     }
 
     res.render('pages/other',
